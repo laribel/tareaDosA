ea6971e98ef5dbeede1872973958306365251ed8
Laribel <laribelcorderol@gmail.com>
2025-03-19 10:12:21 -0600

    first commit

---
+++ android/app/src/main/java/ionic/tareados/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.tareados;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.tareados',
+  appName: 'tareados',
+  webDir: 'dist'
+};
+
+export default config;
